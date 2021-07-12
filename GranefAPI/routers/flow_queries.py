87ce5cffd652c691d64adac7dbe88f84c9e11496
Aneta Jablunková <469018@mail.muni.cz>
2021-07-12 20:05:39 +0200

    feat: add more filters and time ranges

--- GranefAPI/routers/flow_queries.py
+++ GranefAPI/routers/flow_queries.py
@@ -95,6 +95,99 @@ def connection_info(from_timestamp: str, to_timestamp: str, type: str = "json", 
     return {"response": json.loads(result)}
 
 
+@router.get("/host_connections_info", response_model=query_models.GeneralResponse, summary="Return host's connections in a specified time interval.")
+def connection_info(host_ip: str, from_timestamp: str, to_timestamp: str, type: str = "json", layout: str = "sfdp"):
+
+    if not validation.is_address(host_ip):
+        raise HTTPException(
+            status_code = 400,
+            detail = f"Given address {host_ip} is not valid IPv4, IPv6 address, or CIDR notation."
+        )
+
+    dgraph_client = DgraphClient()
+    
+    query_variables = {host_ip: "string", from_timestamp: "string", to_timestamp: "string"}
+    query = f"""{{
+        getConnections(func: eq(host.ip, "{host_ip}")) {{
+            label : host.ip
+            host.ip
+            host.originated @filter(ge(connection.Start_Time_first_seen, "{from_timestamp}") AND le(connection.End_Time_last_seen, "{to_timestamp}")) {{
+                expand(connection)
+                ~host.responded {{
+                    label : host.ip
+                    host.ip
+                }}
+            }}
+        }}
+    }}"""
+
+    # Perform query and raise HTTP exception if any error occurs
+    try:
+        result = dgraph_client.query(query)
+    except Exception as e:
+        raise HTTPException(
+            status_code = 500,
+            detail = str(e)
+        )
+    return {"response": json.loads(result)}
+
+
+@router.get("/host_protocol_usage", response_model=query_models.GeneralResponse, summary="Return usage of protocol and ports information about the specified host.")
+def connection_info(host_ip: str, type: str = "json", layout: str = "sfdp"):
+
+    if not validation.is_address(host_ip):
+        raise HTTPException(
+            status_code = 400,
+            detail = f"Given address {host_ip} is not valid IPv4, IPv6 address, or CIDR notation."
+        )
+
+    dgraph_client = DgraphClient()
+    
+    query_variables = {host_ip: "string"}
+    query = f"""{{
+        getProtocolsUsage(func: eq(host.ip, "{host_ip}")) @normalize {{
+            label : host.ip
+            host.ip
+            host.originated @groupby(connection.Protocol) {{
+                originated_count : count(uid)
+            }}
+            host.responded @groupby(connection.Protocol) {{
+                responded_count : count(uid)
+            }}
+        }}
+        getRespPortUsage(func: eq(host.ip, "{host_ip}")) @normalize {{
+            label : host.ip
+            host.ip
+            host.originated @groupby(connection.Destination_Port) {{
+                originated_count : count(uid)
+            }}
+            host.responded @groupby(connection.Destination_Port) {{
+                responded_count : count(uid)
+            }}
+        }}
+        getOrigPortUsage(func: eq(host.ip, "{host_ip}")) @normalize {{
+            label : host.ip
+            host.ip
+            host.originated @groupby(connection.Source_Port) {{
+            originated_count : count(uid)
+            }}
+            host.responded @groupby(connection.Port) {{
+            responded_count : count(uid)
+            }}
+        }}
+    }}"""
+
+    # Perform query and raise HTTP exception if any error occurs
+    try:
+        result = dgraph_client.query(query)
+    except Exception as e:
+        raise HTTPException(
+            status_code = 500,
+            detail = str(e)
+        )
+    return {"response": json.loads(result)}
+
+
 @router.get("/communicated_hosts", response_model=query_models.GeneralResponse, summary="List of all hosts that communicate with specified host.")
 def communicated_hosts(host_ip: str, type: str = "json", layout: str = "sfdp"):
 
@@ -108,7 +201,7 @@ def communicated_hosts(host_ip: str, type: str = "json", layout: str = "sfdp"):
     
     query_variables = {host_ip: "string"}
     query = f"""{{
-        getCommunicatedHosts(func: allof(host.ip, cidr, "{host_ip}")) {{ 
+        getCommunicatedHosts(func: allof(host.ip, cidr, "{host_ip}")) {{
             label : host.ip
             host.ip
             host.communicated {{
@@ -117,6 +210,43 @@ def communicated_hosts(host_ip: str, type: str = "json", layout: str = "sfdp"):
                 host.hostname {{
                     label: hostname.type
                     hostname.name
+                }}
+            }}
+        }} 
+    }}"""
+
+    # Perform query and raise HTTP exception if any error occurs
+    try:
+        result = dgraph_client.query(query, query_variables)
+    except Exception as e:
+        raise HTTPException(
+            status_code = 500,
+            detail = str(e)
+        )
+    return {"response": json.loads(result)}
+
+
+@router.get("/communicated_hostnames_count", response_model=query_models.GeneralResponse, summary="Count all hostnames, which host communicates with the specified host.")
+def communicated_hosts(host_ip: str, type: str = "json", layout: str = "sfdp"):
+
+    if not validation.is_address(host_ip):
+        raise HTTPException(
+            status_code = 400,
+            detail = f"Given address {host_ip} is not valid IPv4, IPv6 address, or CIDR notation."
+        )
+
+    dgraph_client = DgraphClient()
+    
+    query_variables = {host_ip: "string"}
+    query = f"""{{
+        getCommunicatedHosts(func: allof(host.ip, cidr, "{host_ip}")) {{
+            label : host.ip
+            host.ip
+            host.communicated {{
+                label : host.ip
+                host.ip
+                host.hostname @filter(eq(hostname.type, $protocol)) @groupby(hostname.name) {{
+                    count(uid)
                 }}
             }}
         }} 
