1162d9912929f415b7b21f32fb8d2e7198dba35a
Milan Cermak <cermak@ics.muni.cz>
2021-09-03 10:27:21 +0200

    Graph queries

--- GranefAPI/main.py
+++ GranefAPI/main.py
@@ -46,7 +46,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 # Custom modules of Granef API
 from utilities.dgraph_client import DgraphClient
-from routers import general_queries, overview_queries
+from routers import general_queries, overview_queries, graph_queries
 
 
 # Application definition ("description" key may be added too).
@@ -57,6 +57,7 @@ app = FastAPI(
 
 # Load API routers
 app.include_router(general_queries.router, tags=["General"])
+app.include_router(graph_queries.router, prefix="/graph", tags=["Graph queries"])
 app.include_router(overview_queries.router, prefix="/overview", tags=["Overview queries"])
 
 
---
+++ GranefAPI/routers/graph_queries.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# Granef -- graph-based network forensics toolkit
+# Copyright (C) 2020-2021  Milan Cermak, Institute of Computer Science of Masaryk University
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+
+"""
+Definition of queries providing an interactivity for graph analysis.
+"""
+
+# Common Python modules
+import json
+
+# FastAPI modules
+from fastapi import APIRouter
+from fastapi import HTTPException
+
+# GranefAPI
+from models import query_models
+from utilities import validation, preprocessing
+from utilities.dgraph_client import DgraphClient
+
+
+# Initialize FastAPI router
+router = APIRouter()
+
+
+@router.get("/node_attributes",
+    response_model=query_models.GeneralResponseList, 
+    summary="Get all node attributes for given nodes uid")
+def node_attributes(uids: str) -> dict:
+    """
+    Get all attributes of given nodes uid (separated by comma).
+    """
+    dgraph_client = DgraphClient()
+
+    query = f"""{{
+        node_attributes(func: uid({uids})) {{
+            expand(_all_)
+        }} 
+    }}"""
+
+    # Perform query and raise HTTP exception if any error occurs
+    result = json.loads(dgraph_client.query(preprocessing.add_default_attributes(query)))
+    return {"response": result["node_attributes"]}
