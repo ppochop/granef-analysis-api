04fe1441cc74684e24a83de6b5bd61ff6c2ff9f4
Milan Cermak <cermak@ics.muni.cz>
2022-01-13 14:14:22 +0100

    Fix

--- GranefAPI/routers/graph_queries.py
+++ GranefAPI/routers/graph_queries.py
@@ -36,15 +36,15 @@ def node_attributes(uid: str, return_type: str = "json", graph_layout: str = "sf
     response_model=models.ResponseModel, 
     summary="Get all node neighbors for a given node uid")
 def node_neighbors(uid: str, types: str, return_type: str = "json", graph_layout: str = "sfdp"):
-    query_header = "query getAllNodeNeighbors($uid: string, $types: string)"
+    query_header = "query getAllNodeNeighbors($uid: string)"
     query_body = """
     {
-        neighbors(func: uid($uid)) {
+        getAllNodeNeighbors(func: uid($uid)) {
             expand(_all_) {
-                expand($types)
+                expand({types})
             }   
         }
     }
-    """
-    variables_dict = {"$uid": uid, "$types": types}  
+    """.format(types=types)
+    variables_dict = {"$uid": uid}  
     return qutils.handle_query(query_body=query_body, query_header=query_header, variables=variables_dict, type=return_type, layout=graph_layout)
