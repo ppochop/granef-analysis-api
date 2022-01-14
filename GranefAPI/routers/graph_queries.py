13b554441989109446bef018e90539a5d75281c2
Milan Cermak <cermak@ics.muni.cz>
2022-01-14 13:57:30 +0100

    Additional label

--- GranefAPI/routers/graph_queries.py
+++ GranefAPI/routers/graph_queries.py
@@ -43,6 +43,7 @@ def node_neighbors(uid: str, types: str, return_type: str = "json", graph_layout
             }}
             ioc {{
                 expand({types}) {{
+                    label: misp.info
                     expand({types})
                 }}
             }}
